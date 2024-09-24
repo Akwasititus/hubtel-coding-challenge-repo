b376c851a8bb0a305b5275ac5671ae4b96fe48ea
Akwasititus <www.ayamgatitus@gmail.com>
2024-09-24 12:35:51 +0000

    feat: added test cases

--- test/custom_card_test.dart
+++ test/custom_card_test.dart
@@ -1,15 +1,19 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:untitled/widgets/custom_card.dart';
+import 'package:hubtel_coding_challenge/widgets/history_card_model.dart';
+import 'package:flutter/material.dart';
 
 void main(){
   test("creating a custom card", (){
-    final card = CustomnCard(
+    final card = CustomCardModel(
         status: 'successful',
         time: '14:45 PM',
         bankName: "Emmanuel Rockson",
         phone: "0545504545",
         bankLogo: "bankLogo",
-        amount: 600
+        amount: 600,
+        identityAlias: 'cool your heard wai',
+        color: Colors.white,
+        identity: 'person'
     );
 
     expect(card.status, 'successful');
@@ -17,6 +21,9 @@ void main(){
     expect(card.bankName, 'Emmanuel Rockson');
     expect(card.phone, '0545504545');
     expect(card.bankLogo, 'bankLogo');
+    expect(card.identityAlias, 'cool your heard wai');
+    expect(card.color, Colors.white);
+    expect(card.identity, 'person');
     expect(card.amount, 600);
   });
 }
